aa5862ad3d49bb25e8ab2fec5b374bf66a8f62c0
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-04-09 07:27:20 -0400

    Update install-cluster.sh

--- aws-eks-cluster/install-cluster.sh
+++ aws-eks-cluster/install-cluster.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 eksctl create  cluster -f cluster.yaml
 eksctl utils associate-iam-oidc-provider --cluster=weclouddata --region us-east-1 --approve
+eksctl create addon --name aws-ebs-csi-driver --cluster weclouddata --service-account-role-arn arn:aws:iam::$(aws sts get-caller-identity --query Account --output text):role/AmazonEKS_EBS_CSI_DriverRole --force
+
 #aws eks update-kubeconfig   --region us-east-1 --name weclouddata
 #eksctl get iamidentitymapping --cluster weclouddata --region us-east-1
 #pip3 install awscli --upgrade --user
