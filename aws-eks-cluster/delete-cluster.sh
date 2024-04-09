4118cdf4de481130c8df4ca845375bb06f062048
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-04-08 21:53:49 -0400

    Update delete-cluster.sh

--- aws-eks-cluster/delete-cluster.sh
+++ aws-eks-cluster/delete-cluster.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+eksctl delete addon --cluster weclouddata  --name aws-ebs-csi-driver --preserve
 eksctl delete cluster --region=us-east-1 --name=weclouddata --disable-nodegroup-eviction
